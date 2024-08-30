ddbbd50a094fe4fc5e9f5d9b9a01df630c7f6ffd
Unlimited Coders <unlimitedcoders@Unlimiteds-Mac-mini.local>
2024-08-30 12:57:20 +0200

    Handling errors

--- index.js
+++ index.js
@@ -14,78 +14,73 @@ const db = new pg.Client({
 });
 
 db.connect()
-
+var error = []
+var errorOne = []
 var visitedCountries = []
 
-
+async function checkVisisted(req,response,error,errorOne) {
+  console.log(error)
+   
+    db.query("SELECT country_code FROM visited_countries",(err,res)=>{
+    console.log(error)
+    visitedCountries = res.rows
+    let total = visitedCountries.length
+    
+    let countryCodes = visitedCountries.map(country => country.country_code);
+   
+    response.render("index.ejs",{countries:countryCodes,total,error:error,errorOne:errorOne});
+    
+  });
+}
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
-   
-    app.get("/", async (req, response) => {
-     
-      db.query("SELECT country_code FROM visited_countries",(err,res)=>{
-        
-      visitedCountries = res.rows
-      let total = visitedCountries.length
-    
-      let countryCodes = visitedCountries.map(country => country.country_code);
-     
-      response.render("index.ejs",{countries:countryCodes,total});
-      
-          
-    
-    });
- 
+app.get("/", async (req, response) => {
+checkVisisted(req,response,error,errorOne)
+
 });
   
-      app.post("/add", async (req, response) => {
-        let addedCountry = req.body.country
-       
-        
-        db.query("SELECT country_code FROM countries WHERE country_name ILIKE ($1)",[addedCountry],(err, resp)=>{
-          visitedCountries = resp.rows
-          let countryCodes = visitedCountries.map(country => country.country_code);
-          console.log(countryCodes)
-        
-             db.query("INSERT INTO visited_countries(country_code) VALUES ($1) ",[countryCodes[0]],(err, res)=>{ 
-
-              if(err){
-               
-               
-                db.query("SELECT country_code FROM visited_countries",(err,res)=>{
-                 
-                    let error = {}
-                    visitedCountries = res.rows
-                    let total = visitedCountries.length
-                    
-                    let countryCodes = visitedCountries.map(country => country.country_code);
-                   
-                    response.render("index.ejs",{countries:countryCodes,total,error:error});
-                  
-               });
-                
-              } else {
-                
-                response.redirect('/')
-               
-                } 
-
-              });
-       
-         })
+app.post("/add", async (req, response) => {
+  let addedCountry = req.body.country
+  
+  
+  db.query("SELECT country_code FROM countries WHERE country_name ILIKE ($1)",[addedCountry],(err, resp)=>{
+    console.log(addedCountry)
     
+    if(resp.rows.length <= 0){
+    errorOne.push(1)
+    checkVisisted(req,response,error,errorOne)
+    errorOne = []
+      
+    }
+    else {
+      visitedCountries = resp.rows
+      let countryCodes = visitedCountries.map(country => country.country_code);
+      console.log(countryCodes)
+        db.query("INSERT INTO visited_countries(country_code) VALUES ($1) ",[countryCodes[0]],(err, res)=>{ 
+
+        if(err){
+          
+        error.push(1)
+        checkVisisted(req,response,error,errorOne)
+        error = []
+          
+        } else {
+          
+          response.redirect('/')
+          } 
+
+        });
+      }
     })
-  
+})
 
-
- 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-  
+console.log(`Server running on http://localhost:${port}`);
+
 });
 
 
